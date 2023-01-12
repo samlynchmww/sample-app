f9c8bdd778eb55089b5267e7be3d31de35d3bf5c
saml <saml@manualww.com>
2023-01-12 09:28:02 -0500

    sample app skeleton created

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+# returns the full title on a per page basis
+
+module ApplicationHelper
+    def full_title(page_title='')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
+        else
+            "#{page_title} | #{base_title}"
+        end
+    end
+end
